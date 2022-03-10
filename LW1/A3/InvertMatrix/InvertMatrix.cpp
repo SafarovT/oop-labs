ff954b71267d52663cbfe94d63d43a0da62f17a8
Krasilnikov30 <salarovsmv@gmail.com>
2022-03-10 18:44:48 +0300

    Update InvertMatrix.cpp

--- LW1/A3/InvertMatrix/InvertMatrix.cpp
+++ LW1/A3/InvertMatrix/InvertMatrix.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <optional>
+#include <string>
 
 struct Args
 {
@@ -8,10 +10,50 @@ struct Args
 
 std::optional<Args> ParseArgs(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		std::cout << "Invalid argument count\n"
+			<< "Usage: replace.exe <MatrixFile.txt>\n";
+		return std::nullopt;
+	}
+	std::ifstream inputFile;
+	inputFile.open(argv[1]);
 
+	if (!inputFile.is_open())
+	{
+		std::cout << "Failed to open file for reading \n";
+		return std::nullopt;
+	}
+
+	Args args;
+	int matrix[3][3];
+
+	int inValue;
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			if (inputFile >> inValue)
+			{
+				matrix[i][j] = inValue;
+			}
+			else
+			{
+				std::cout << "Enter a correct matrix with numbers";
+				return std::nullopt;
+			}
+		}
+	}
+
+	args.matrix = matrix;
+
+	if (inputFile.bad())
+	{
+		std::cout << "Failed to read data from input file \n";
+		return std::nullopt;
+	}
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
 }
