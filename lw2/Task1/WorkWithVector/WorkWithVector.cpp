8aa55f2ea0c6bfde0cfd5aae1fbe84f5cb06a92a
krasilnikov30 <salarovsmv@gmail.com>
2022-03-31 21:14:50 +0300

    Продолжил работу над вектором

--- lw2/Task1/WorkWithVector/WorkWithVector.cpp
+++ lw2/Task1/WorkWithVector/WorkWithVector.cpp
@@ -2,21 +2,52 @@
 #include <algorithm>
 #include <vector>
 
-void ReadVector(std::vector<double> vector)
+void ReadVector(std::vector<int> vector)
 {
-    double inValue;
-    while (std::cin >> inValue)
+    int inValue;
+    while (std::cin >> inValue && std::cin.eof())
     {
         vector.push_back(inValue);
     }
 }
 
+void MultOnMinValue(std::vector<int> vector)
+{
+    int minValue = vector[0];
+    std::cout << "there";
+    for (int element : vector)
+    {
+        if (element < minValue)
+        {
+            minValue = element;
+        }
+    }
+
+    for (int element : vector)
+    {
+        element *= minValue;
+    }
+
+    /*std::for_each(vector.begin(), vector.end(),
+        [](int i) {i *= minValue;}
+    );*/
+}
+
+void PrintVector(std::vector<int> vector)
+{
+    std::sort(vector.begin(), vector.end());
+    copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+}
+
 int main()
 {
-    std::vector<double> vector;
-    ReadVector(vector);
+    std::vector<int> vector;
 
-    double minValue = std::min_element(vector[0], vector[2]);
+    ReadVector(vector);
+    std::cout << "Readed\n";
+    MultOnMinValue(vector);
+    std::cout << "Multed\n";
+    PrintVector(vector);
 
     return 0;
 }
