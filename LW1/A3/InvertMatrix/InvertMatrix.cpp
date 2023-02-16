4c88df4f71a43770c8a495688689c3a1295624f4
Safarov_Tahir <salarovsmv@gmail.com>
2023-02-16 11:05:23 +0300

    Сделал InvertMatrix

--- LW1/A3/InvertMatrix/InvertMatrix.cpp
+++ LW1/A3/InvertMatrix/InvertMatrix.cpp
@@ -57,45 +57,94 @@ bool OpenFile(std::ifstream& inputFile, std::string filePath)
 	return true;
 }
 
-double FoundDeterminant2By2Matrix(double matrix[2][2])
+void WriteMatrix(double matrix[3][3])
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			std::cout << round(matrix[i][j] * 1000) / 1000 << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+double GetDeterminant2By2Matrix(double matrix[2][2])
 {
 	return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
 }
 
+double GetDeterminantOfRemainderMatrix(double matrix[3][3], int x, int y)
+{
+	double remainderMatrix[2][2];
+	for (int i = 0, iRemainderMatrix = 0; i < 3; ++i)
+	{
+		if (i != y)
+		{
+			for (int j = 0, jRemainderMatrix = 0; j < 3; ++j)
+			{
+				if (j != x)
+				{
+					remainderMatrix[iRemainderMatrix][jRemainderMatrix] = matrix[i][j];
+					jRemainderMatrix++;
+				}
+			}
+			iRemainderMatrix++;
+		}
+	}
 
-double FoundDeterminant3By3Matrix(double matrix[3][3])
+	return GetDeterminant2By2Matrix(remainderMatrix);
+}
+
+double GetDeterminant3By3Matrix(double matrix[3][3])
 {
 	double determinant = 0;
 
 	for (int i = 0; i < 3; ++i)
 	{
-		double tempMatrix[2][2];
-		for (int j = 1; j < 3; ++j)
-		{
-			for (int k = 0, x = 0; k < 3; ++k)
-			{
-				if (k != i)
-				{
-					tempMatrix[j - 1][x] = matrix[j][k];
-					x++;
-				}
-			}
-		}
-		determinant += pow(-1, i) * matrix[0][i] * FoundDeterminant2By2Matrix(tempMatrix);
+		determinant += pow(-1, i) * matrix[0][i] * GetDeterminantOfRemainderMatrix(matrix, i, 0);
 	}
 
 	return determinant;
 }
 
-void InverMatrix(double matrix[3][3])
+void FindTranspose(double matrix[3][3], double resultMatrix[3][3])
 {
-	double determinant = FoundDeterminant3By3Matrix(matrix);
+	for (int i = 0, y = 0; i < 3; ++i)
+	{
+		for (int j = 0, x = 0; j < 3; ++j)
+		{
+			resultMatrix[j][i] = pow(-1, i + j) * GetDeterminantOfRemainderMatrix(matrix, j, i);
+		}
+	}
+}
+
+void MultMatrixOnNumber(double matrix[3][3], double multiplier)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			matrix[i][j] *= multiplier;
+		}
+	}
+}
+
+void InvertMatrix(double matrix[3][3])
+{
+	double determinant = GetDeterminant3By3Matrix(matrix);
 	if (determinant == 0)
 	{
 		std::cout << "Impossible to invert matrix: determinant is equal to 0\n";
 
 		return;
 	}
+
+	double transpose[3][3];
+	FindTranspose(matrix, transpose);
+	MultMatrixOnNumber(transpose, 1/determinant);
+	WriteMatrix(transpose);
+
 
 }
 
@@ -121,7 +170,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	InverMatrix(matrix);
+	InvertMatrix(matrix);
 	
 	return 0;
 }
--- LW1/A3/InvertMatrix/Tests.bat
+++ LW1/A3/InvertMatrix/Tests.bat
@@ -4,11 +4,28 @@ REM ѕри запуске без параметров ожидаетс€ ненулевой код возврата
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
 
-%PROGRAM% Matrix1.txt >> "%TEMP%\1TestResultWithMatrix1.txt"
+REM ¬вод некоректной матрицы с символами
+%PROGRAM% MatrixWithLetters.txt
+if NOT ERRORLEVEL 1 goto err
+
+REM ¬вод некоректной матрицы с недостаточным количеством значений
+%PROGRAM% MatrixLessNumbers.txt
+if NOT ERRORLEVEL 1 goto err
+
+del "%TEMP%\TestingResult1.txt
+del "%TEMP%\TestingResult2.txt
+
+REM ѕроверка на корректное нахождение обратной матрицы с корректной изначальной матрицой
+%PROGRAM% Matrix1.txt >> "%TEMP%\TestingResult1.txt"
 if ERRORLEVEL 1 goto err
-rem fc.exe "%TEMP%\Matrix12A" Matrix12A.txt
-rem del "%TEMP%\Matrix12A.txt"
-rem if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\TestingResult1.txt" Matrix1A.txt
+if ERRORLEVEL 1 goto err
+
+REM ѕроверка на определение нулевого определител€
+%PROGRAM% MatrixWithZeroDeterminant.txt >> "%TEMP%\TestingResult2.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\TestingResult2.txt" ZeroDeterminantOutput.txt
+if ERRORLEVEL 1 goto err
 
 echo OK
 exit 0
