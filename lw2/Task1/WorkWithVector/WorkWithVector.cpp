c8acd0cffa98e5273666c065e24b79b10a09110a
Safarov_Tahir <salarovsmv@gmail.com>
2023-03-06 14:06:08 +0300

    Дописал первое задание второй лабы

--- lw2/Task1/WorkWithVector/WorkWithVector.cpp
+++ lw2/Task1/WorkWithVector/WorkWithVector.cpp
@@ -2,52 +2,43 @@
 #include <algorithm>
 #include <vector>
 
-void ReadVector(std::vector<int> vector)
+enum ProgramEndCode
+{
+    Success = 0, Error = 1
+};
+
+using VectorElementType = int;
+using VectorType = std::vector<VectorElementType>;
+
+void ReadVector(VectorType& vector)
 {
     int inValue;
-    while (std::cin >> inValue && std::cin.eof())
+    while (std::cin >> inValue)
     {
         vector.push_back(inValue);
     }
 }
 
-void MultOnMinValue(std::vector<int> vector)
+void MultVectorOnMinValue(VectorType& vector)
 {
-    int minValue = vector[0];
-    std::cout << "there";
-    for (int element : vector)
-    {
-        if (element < minValue)
-        {
-            minValue = element;
-        }
-    }
-
-    for (int element : vector)
-    {
-        element *= minValue;
-    }
-
-    /*std::for_each(vector.begin(), vector.end(),
-        [](int i) {i *= minValue;}
-    );*/
+    VectorType::iterator minElementIterator = std::min_element(vector.begin(), vector.end());
+    auto multElementOnMinValue = [](VectorElementType element, VectorType::iterator minElementIterator) { return element * *minElementIterator; };
+    std::transform(vector.begin(), vector.end(), vector.begin(), multElementOnMinValue);
 }
 
-void PrintVector(std::vector<int> vector)
+void PrintVectorSorted(const VectorType& vector)
 {
     std::sort(vector.begin(), vector.end());
-    copy(vector.begin(), vector.end(), std::ostream_iterator<int>(std::cout, " "));
+    copy(vector.begin(), vector.end(), std::ostream_iterator<size_t>(std::cout, " "));
 }
 
 int main()
 {
-    std::vector<int> vector;
+    VectorType vector;
 
     ReadVector(vector);
-    std::cout << "Readed\n";
-    MultOnMinValue(vector);
-    std::cout << "Multed\n";
-    PrintVector(vector);
+    MultVectorOnMinValue(vector);
+    PrintVectorSorted(vector);
 
-    return 0;
+    return ProgramEndCode::Success;
 }
