8e3321b0f17cc2d261fb5765764c52f19a9a7286
Safarov_Tahir <salarovsmv@gmail.com>
2023-04-30 21:54:16 +0300

    Начал делать калькулятор

---
+++ lw3/task2/Calculator/CalculatorTests/CalculatorTests.cpp
@@ -0,0 +1,16 @@
+﻿#include "../../../../external/catch2/catch.hpp"
+#include "../Calculator/Calculator.h"
+
+SCENARIO("Scenario")
+{
+	GIVEN("GIVEN")
+	{
+		WHEN("WHEN")
+		{
+			THEN("then")
+			{
+				CHECK(true)
+			}
+		}
+	}
+}
